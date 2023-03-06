2bd9703ed88d73c6a892ada811b2a597567aa872
Gianni Massi <me@giannimassi.it>
2023-03-06 17:12:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 
-echo ´
+echo '
  ██████╗ ██╗ █████╗ ███╗   ██╗███╗   ██╗██╗    ███╗   ███╗ █████╗ ███████╗███████╗██╗              ██╗   ██╗███╗   ██╗██╗██╗  ██╗    ███████╗███████╗████████╗██╗   ██╗██████╗ 
 ██╔════╝ ██║██╔══██╗████╗  ██║████╗  ██║██║    ████╗ ████║██╔══██╗██╔════╝██╔════╝██║              ██║   ██║████╗  ██║██║╚██╗██╔╝    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
 ██║  ███╗██║███████║██╔██╗ ██║██╔██╗ ██║██║    ██╔████╔██║███████║███████╗███████╗██║    █████╗    ██║   ██║██╔██╗ ██║██║ ╚███╔╝     ███████╗█████╗     ██║   ██║   ██║██████╔╝
 ██║   ██║██║██╔══██║██║╚██╗██║██║╚██╗██║██║    ██║╚██╔╝██║██╔══██║╚════██║╚════██║██║    ╚════╝    ██║   ██║██║╚██╗██║██║ ██╔██╗     ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
 ╚██████╔╝██║██║  ██║██║ ╚████║██║ ╚████║██║    ██║ ╚═╝ ██║██║  ██║███████║███████║██║              ╚██████╔╝██║ ╚████║██║██╔╝ ██╗    ███████║███████╗   ██║   ╚██████╔╝██║     
  ╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚═╝    ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝               ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     
-                                                                                                                                                                               ´
+                                                                                                                                                                               '
+
+
+## Download this repo and de-compress at dotfiles
+
 ## Install Brew (works both on macos and linux)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+## Run these two commands in your terminal to add Homebrew to your PATH:
+#(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/gianni/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+
+# Install dependencies for each OS
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo apt-get install build-essential
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+fi
+
+# Install everything!
+brew tap jtyr/repo # needed for gbt
+brew install gcc tmux stow direnv zsh gbt git
+brew install --cask visual-studio-code
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install golang
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm 1.20.1
+
+
+# Install rust and packages built with rust
+brew install rust
+cargo install git-absorb
 
